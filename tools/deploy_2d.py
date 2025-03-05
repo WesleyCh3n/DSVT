23d3578f0a413e3e2f5332aac6448739fbe0de9e
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-03-05 11:38:36 +0800

    fix(deploy): 2d input

--- tools/deploy_2d.py
+++ tools/deploy_2d.py
@@ -1,5 +1,9 @@
 # type: ignore
 import os
+import re
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 import torch
 import torch.onnx
@@ -11,11 +15,13 @@ from pcdet.utils import common_utils
 
 # load model
 cfg_file = "./cfgs/dsvt_models/dsvt_plain_1f_onestage.yaml"
+# cfg_file = "./cfgs/waymo_models/pv_rcnn.yaml"
 cfg_from_yaml_file(cfg_file, cfg)
 if os.path.exists("./deploy_files") == False:
     os.mkdir("./deploy_files")
 log_file = "./deploy_files/log_trt.log"
 logger = common_utils.create_logger(log_file, rank=0)
+
 test_set, test_loader, sampler = build_dataloader(
     dataset_cfg=cfg.DATA_CONFIG,
     class_names=cfg.CLASS_NAMES,
@@ -26,26 +32,23 @@ test_set, test_loader, sampler = build_dataloader(
     training=False,
 )
 
-
 model = build_network(
     model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=test_set
 )
 model.eval().cuda()
-print(model.backbone_2d)
+backbone_2d_name = model.backbone_2d.__class__.__name__
 
+input = {
+    "spatial_features": torch.randn(
+        1, *[cfg.MODEL.MAP_TO_BEV.NUM_BEV_FEATURES for _ in range(3)]
+    ).cuda()
+}
+
+camel2snake = lambda s: re.sub(r"([a-z])([A-Z])", r"\1_\2", s).lower()
 torch.onnx.export(
     model.backbone_2d,
-    (
-        {
-            "spatial_features": torch.randn(
-                1,
-                cfg.MODEL.MAP_TO_BEV.NUM_BEV_FEATURES,
-                *cfg.MODEL.MAP_TO_BEV.INPUT_SHAPE[:2],
-            ).cuda()
-        },
-        {},
-    ),
-    "./deploy_files/bev_backbone.onnx",
+    (input, {}),
+    f"./deploy_files/{camel2snake(backbone_2d_name)}.onnx",
     input_names=["spatial_features"],
     output_names=["spatial_features_2d"],
     dynamic_axes={
